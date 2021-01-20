823b2230942bbdccf4a935858399b6fdb1d901b6
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-01-19 22:33:25 -0800

    Show most voted with useEffect

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = props => {
@@ -7,26 +7,51 @@ const App = props => {
     setSelected(randomItem);
   };
 
+  const checkMostVoted = () => {
+    let max = -1000,
+      maxIndex = -1;
+    console.log(votes);
+    for (let prop in votes) {
+      console.log(prop + ' ' + votes[prop]);
+      if (votes[prop] > max) {
+        maxIndex = prop;
+        max = votes[prop];
+      }
+    }
+    return maxIndex;
+  };
+
   const voteUp = () => {
     const copy = { ...votes };
     copy[selected]++;
-    setVotes(copy);
+    setVotes(copy, () => {});
+    setMostVoted(checkMostVoted());
   };
 
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(points);
+  const [mostVoted, setMostVoted] = useState(checkMostVoted());
+
+  useEffect(() => {
+    setMostVoted(checkMostVoted());
+    console.log('votes', votes);
+    console.log('votes', props.anecdotes);
+  }, [votes]);
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{props.anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={() => voteUp()}>vote</button>
       <button onClick={() => getRandom()}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{props.anecdotes[mostVoted]}</p>
     </div>
   );
 };
 
-const points = { 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5 };
+const points = { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 };
 
 const anecdotes = [
   '0 If it hurts, do it more often',
